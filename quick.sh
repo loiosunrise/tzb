058c5834686ff2ec39e2f94bda2e3aeb6db7fb2b
loiosunrise <loiosunrisek@gmail.com>
2024-08-07 19:41:14 +0800

    change Dockerfiles and quick.sh

--- quick.sh
+++ quick.sh
@@ -1,3 +1,8 @@
 TAG=${1:-latest}
+sh build.sh base ${TAG}
+sh build.sh test ${TAG}
+sh build.sh upload ${TAG}
+docker tag tzb-test:${TAG} registry.cn-hangzhou.aliyuncs.com/tzb-sincos/tzb-test:${TAG}
+docker tag tzb-upload:${TAG} registry.cn-hangzhou.aliyuncs.com/tzb-sincos/tzb-upload:${TAG}
 docker push registry.cn-hangzhou.aliyuncs.com/tzb-sincos/tzb-test:${TAG}
 docker push registry.cn-hangzhou.aliyuncs.com/tzb-sincos/tzb-upload:${TAG}
